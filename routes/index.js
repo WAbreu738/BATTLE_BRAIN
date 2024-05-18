e8e1904af8a863e23a0ab0fc5776749181c6ca9e
WAbreu738 <139182882+WAbreu738@users.noreply.github.com>
2024-05-18 11:31:30 -0400

    Delete routes directory

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router()
-
-const view_routes = require('./view_routes')
-//other routes
-
-// router.use('/', [view_routes, other routes])
-router.use('/', view_routes)
-
-module.exports = router
