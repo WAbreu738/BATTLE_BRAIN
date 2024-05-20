662ddca1518a960a95bc7c859bf95ad74bc338d3
Blackbox19 <trevorirwin819@gmail.com>
2024-05-20 16:59:50 -0400

    Trevor, added basic client routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import { useState } from "react";
-// import TriviaComponent from "./Main_components/Trivia_Test";
-// import HomeWindow from "./Main_components/Home_Window/Home_Window";
-// import BattleMode from "./Main_components/BattleMode_Window/BattleMode_Window";
+import HomeWindow from "./Main_components/Home_Window/Home_Window";
+import BattleMode from "./Main_components/BattleMode_Window/BattleMode_Window";
 import LobbyWindow from "./Main_components/Lobby_Window/Lobby_Window";
-// import CategoryWindow from "./Main_components/Category_Window/Category";
-// import "./index.scss";
-import "./App.css";
+import CategoryWindow from "./Main_components/Category_Window/Category";
 
 function App() {
   return (
-    <>
-      <div className="">
-        {/* <TriviaComponent /> */}
-        {/* <HomeWindow /> */}
-        {/* <BattleMode /> */}
-        <LobbyWindow />
-        {/* <CategoryWindow /> */}
+    <main>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomeWindow />} />
+          <Route path="/lobby" element={<LobbyWindow />} />
+          <Route path="/catagory" element={<CategoryWindow />} />
+          <Route path="/battle" element={<BattleMode />} />
+        </Routes>
       </div>
-    </>
+    </main>
   );
 }
 
--- client/src/Main_components/Lobby_Window/components/start_btn.jsx
+++ client/src/Main_components/Lobby_Window/components/start_btn.jsx
@@ -1,9 +1,14 @@
+import { NavLink } from "react-router-dom";
+
 const StartBtn = () => {
   return (
     <div className="text-center">
-      <button className="bg-green-500 text-white py-6 px-12 rounded-lg text-2xl shadow-lg hover:bg-green-700">
+      <NavLink
+        to="/battle"
+        className="bg-green-500 text-white py-6 px-12 rounded-lg text-2xl shadow-lg hover:bg-green-700"
+      >
         START
-      </button>
+      </NavLink>
     </div>
   );
 };
